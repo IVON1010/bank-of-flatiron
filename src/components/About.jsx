dc3a508312c2dc693209312f778d631ff9a6a0c3
yvonnendungu <ndunguyvonne10@gmail.com>
2024-04-28 01:09:56 +0300

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+export default function About() {
+  return (
+    <table>
+        <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+        </tr>
+    </table>
+  )
+}
